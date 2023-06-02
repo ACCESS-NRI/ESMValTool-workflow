79fa1e0c693b765154d5a3444c02637cbe76d4e1
rbeucher <romain.beucher@anu.edu.au>
2023-06-02 11:22:51 +1000

    Update generate.py

--- utilities/generate.py
+++ utilities/generate.py
@@ -4,7 +4,7 @@ import subprocess
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 
-# Name of the conda environment in which esmvaltool is installed
+# Name of the conda enviro-l walltime={walltime}\nnment in which esmvaltool is installed
 env = 'conda/access-med-0.1'
 # Mail notifications when a submitted job fails or finishes
 mail = False
@@ -15,7 +15,7 @@ account = 'iq82'  # Select a compute project to be billed
 # The outputs will be saved in /home/user/<outputs>
 outputs = '/g/data/kj13/admin/ESMValTool/logs'
 # Default Levante computing partition used
-nci_queue = 'normal'
+nci_queue = 'copyq'
 # Default amount of memory used
 memory = '64GB'
 # Default walltime
@@ -27,127 +27,90 @@ config_file = '/g/data/kj13/admin/ESMValTool/.esmvaltool/config-user.yml'
 # List of recipes that require non-default SLURM options set above
 SPECIAL_RECIPES = {
     'recipe_anav13jclim': {
-        'partition': '#SBATCH --partition=compute \n',
-        'time': '#SBATCH --time=08:00:00 \n',
+        'nci_queue': '#PBS -q copyq\n',
+        'walltime': '#PBS -l walltime=08:00:00\n',
     },
     'recipe_bock20jgr_fig_6-7': {
-        'partition': '#SBATCH --partition=shared \n',
-        'time': '#SBATCH --time=48:00:00 \n',
-        'memory': '#SBATCH --mem=50G \n',
+        'nci_queue': '#PBS -q normal\n',
+        'walltime': '#PBS -l walltime=48:00:00 \n',
+        'memory': '#PBS -l mem=50GB \n',
     },
     'recipe_bock20jgr_fig_8-10': {
-        'partition': '#SBATCH --partition=shared \n',
-        'time': '#SBATCH --time=48:00:00 \n',
-        'memory': '#SBATCH --mem=50G \n',
+        'nci_queue': '#PBS -q normal\n',
+        'walltime': '#PBS -l walltime=48:00:00\n',
+        'memory': '#PBS -l mem=50GB \n',
     },
     'recipe_check_obs': {
-        'partition': '#SBATCH --partition=compute \n',
-        'memory': '#SBATCH --constraint=512G \n',
+        'nci_queue': '#PBS -q megamem\n',
+        'memory': '#PBS -l mem=512GB \n',
     },
     'recipe_climate_change_hotspot': {
-        'partition': '#SBATCH --partition=compute \n',
+        'nci_queue': '#PBS -q normal\n',
     },
     'recipe_collins13ipcc': {
-        'partition': '#SBATCH --partition=compute \n',
-        'time': '#SBATCH --time=08:00:00 \n',
-        'memory': '#SBATCH --constraint=512G \n',
-    },
-    'recipe_eady_growth_rate': {
-        'partition': '#SBATCH --partition=compute \n',
-    },
-    'recipe_ecs': {
-        'partition': '#SBATCH --partition=compute \n',
-    },
-    'recipe_ecs_constraints': {
-        'partition': '#SBATCH --partition=compute \n',
+        'nci_queue': '#PBS -q megamem\n',
+        'walltime': '#PBS -l walltime=8:00:00\n',
+        'memory': '#PBS -l mem=512GB \n',
     },
     'recipe_extreme_index': {
-        'partition': '#SBATCH --partition=compute \n',
-    },
-    'recipe_eyring06jgr': {
-        'partition': '#SBATCH --partition=compute \n',
+        'nci_queue': '#PBS -q normal\n',
     },
     'recipe_eyring13jgr_12': {
-        'partition': '#SBATCH --partition=compute \n',
+        'nci_queue': '#PBS -q normal\n',
     },
     'recipe_flato13ipcc_figures_938_941_cmip6': {
-        'partition': '#SBATCH --partition=compute \n',
+        'nci_queue': '#PBS -q normal\n',
     },
     'recipe_ipccwg1ar6ch3_atmosphere': {
-        'partition': '#SBATCH --partition=compute \n',
+        'nci_queue': '#PBS -q normal\n',
     },
     'recipe_ipccwg1ar6ch3_fig_3_9': {
-        'partition': '#SBATCH --partition=shared \n',
-        'time': '#SBATCH --time=15:00:00 \n',
-        'memory': '#SBATCH --mem=150G \n',
+        'nci_queue': '#PBS -q normal\n',
+        'walltime': '#PBS -l walltime=15:00:00\n',
+        'memory': '#PBS -l mem=150GB \n',
     },
     'recipe_ipccwg1ar6ch3_fig_3_19': {
-        'partition': '#SBATCH --partition=compute \n',
+        'nci_queue': '#PBS -q normal\n',
     },
     'recipe_ipccwg1ar6ch3_fig_3_42_a': {
-        'partition': '#SBATCH --partition=compute \n',
-        'time': '#SBATCH --time=08:00:00 \n',
-        'memory': '#SBATCH --constraint=512G \n',
+        'nci_queue': '#PBS -q megamem\n',
+        'walltime': '#PBS -l walltime=08:00:00\n',
+        'memory': '#PBS -l mem=512GB \n',
     },
     'recipe_ipccwg1ar6ch3_fig_3_42_b': {
-        'partition': '#SBATCH --partition=compute \n',
+        'nci_queue': '#PBS -q normal\n',
     },
     'recipe_ipccwg1ar6ch3_fig_3_43': {
-        'partition': '#SBATCH --partition=compute \n',
-    },
-    'recipe_lauer22jclim_fig3-4_zonal': {
-        'partition': '#SBATCH --partition=compute \n',
-    },
-    'recipe_lauer22jclim_fig5_lifrac': {
-        'partition': '#SBATCH --partition=compute \n',
-    },
-    'recipe_meehl20sciadv': {
-        'partition': '#SBATCH --partition=compute \n',
-    },
-    'recipe_mpqb_xch4': {
-        'partition': '#SBATCH --partition=compute \n',
-    },
-    'recipe_perfmetrics_CMIP5': {
-        'partition': '#SBATCH --partition=compute \n',
-    },
-    'recipe_perfmetrics_CMIP5_4cds': {
-        'partition': '#SBATCH --partition=compute \n',
-    },
-    'recipe_perfmetrics_land_CMIP5': {
-        'partition': '#SBATCH --partition=compute \n',
-    },
-    'recipe_russell18jgr': {
-        'partition': '#SBATCH --partition=compute \n',
+        'nci_queue': '#PBS -q normal\n',
     },
     'recipe_schlund20esd': {
-        'partition': '#SBATCH --partition=compute \n',
-        'time': '#SBATCH --time=08:00:00 \n',
-        'memory': '#SBATCH --constraint=512G \n',
+        'nci_queue': '#PBS -q megamem\n',
+        'walltime': '#PBS -l walltime=08:00:00\n',
+        'memory': '#PBS -l mem=512GB \n',
     },
     'recipe_schlund20jgr_gpp_abs_rcp85': {
-        'partition': '#SBATCH --partition=compute \n',
+        'nci_queue': '#PBS -q normal\n',
     },
     'recipe_schlund20jgr_gpp_change_1pct': {
-        'partition': '#SBATCH --partition=compute \n',
+        'nci_queue': '#PBS -q normal\n',
     },
     'recipe_schlund20jgr_gpp_change_rcp85': {
-        'partition': '#SBATCH --partition=compute \n',
+        'nci_queue': '#PBS -q normal\n',
     },
     'recipe_smpi': {
-        'partition': '#SBATCH --partition=compute \n',
+        'nci_queue': '#PBS -q normal\n',
+        'memory': '#PBS -l mem=128GB \n',
     },
     'recipe_smpi_4cds': {
-        'partition': '#SBATCH --partition=compute \n',
+        'nci_queue': '#PBS -q copyq\n',
+        'memory': '#PBS -l mem=128GB \n',
     },
     'recipe_tebaldi21esd': {
-        'partition': '#SBATCH --partition=compute \n',
-        'time': '#SBATCH --time=08:00:00 \n',
+        'nci_queue': '#PBS -q copyq\n',
+        'walltime': '#PBS -l walltime=08:00:00\n',
     },
-    'recipe_wenzel16jclim': {
-        'partition': '#SBATCH --partition=compute \n',
-    },
-    'recipe_wenzel16nat': {
-        'partition': '#SBATCH --partition=compute \n',
+    'recipe_martin18grl': {
+        'walltime': '#PBS -l walltime=08:00:00\n',
     },
 }
 
@@ -209,30 +172,24 @@ def generate_submit():
                 file.write(f'#PBS -l walltime={walltime}\n')
                 file.write(f'#PBS -l mem={memory}\n')
                 file.write('#PBS -l ncpus=1\n')
-            #else:
-            #    file.write(SPECIAL_RECIPES[recipe.stem]['partition'])
-            #    # Time requirements
-            #    # Special time requirements
-            #    if 'walltime' in SPECIAL_RECIPES[recipe.stem]:
-            #        file.write(SPECIAL_RECIPES[recipe.stem]['walltime'])
-            #    # Default
-            #    else:
-            #        file.write(f'#PBS -l walltime={walltime}\n')
-            #    # Memory requirements
-            #    # Full node memory (compute partition)
-            #    if 'compute' in SPECIAL_RECIPES[recipe.stem]['partition']:
-            #        mem_req_levante = '#SBATCH --mem=0\n'
-            #        file.write(mem_req_levante)
-            #        if 'memory' in SPECIAL_RECIPES[recipe.stem]:
-            #            file.write(SPECIAL_RECIPES[recipe.stem]['memory'])
-            #    # Shared nodes (other partitions)
-            #    else:
-            #        # Special memory requirements
-            #        if 'memory' in SPECIAL_RECIPES[recipe.stem]:
-            #            file.write(SPECIAL_RECIPES[recipe.stem]['memory'])
-            #        # Default
-            #        else:
-            #            file.write(f'#SBATCH --mem={memory}\n')
+            else:
+                if 'nci_queue' in SPECIAL_RECIPES[recipe.stem]:
+                    file.write(SPECIAL_RECIPES[recipe.stem]['nci_queue'])
+                else:
+                    file.write(f'#PBS -q {nci_queue}\n')
+                # Time requirements
+                # Special time requirements
+                if 'walltime' in SPECIAL_RECIPES[recipe.stem]:
+                    file.write(SPECIAL_RECIPES[recipe.stem]['walltime'])
+                # Default
+                else:
+                    file.write(f'#PBS -l walltime={walltime}\n')
+                # Special memory requirements
+                if 'memory' in SPECIAL_RECIPES[recipe.stem]:
+                    file.write(SPECIAL_RECIPES[recipe.stem]['memory'])
+                # Default
+                else:
+                    file.write(f'#PBS -l mem={memory}\n')
             if mail:
                 file.write('#PBS -m e \n')
                 file.write('#PBS -M romain.beucher@anu.edu.au \n')
