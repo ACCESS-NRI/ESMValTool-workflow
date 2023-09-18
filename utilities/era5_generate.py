0e92186c400fb3867c17dd2daee8ef93bb1b1a4b
Romain Beucher <romain.beucher@anu.edu.au>
2023-09-19 09:39:49 +1000

    Update ERA5 daily scripts for OBS6

--- utilities/era5_generate.py
+++ utilities/era5_generate.py
@@ -5,34 +5,39 @@ from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 
 
-def generate_pbs_scripts():
+def generate_pbs_scripts(variables):
     """Generate GitHub action scripts."""
     environment = Environment(loader=FileSystemLoader("templates/"))
     template = environment.get_template("launch_recipe_daily_era5_year.pbs")
 
-    for year in range(1990, 2023):
-        filename = f'launch_recipe_daily_era5_{year}.pbs'
-        content = template.render(year=year)
-        with open(f'./ERA5/{filename}', 'w', encoding='utf-8') as file:
-            file.write(content)
+    for var in variables:
+        for year in range(1990, 2024):
+            filename = f'launch_recipe_daily_era5_{var}_{year}.pbs'
+            content = template.render(year=year, variable=var)
+            with open(f'{filename}', 'w', encoding='utf-8') as file:
+                file.write(content)
 
-def generate_recipes():
+def generate_recipes(variables):
     """Generate GitHub action scripts."""
     environment = Environment(loader=FileSystemLoader("templates/"))
     template = environment.get_template("recipe_daily_era5_year.yml")
 
-    for year in range(1990, 2023):
-        filename = f'recipe_daily_era5_{year}.yml'
-        content = template.render(year=year)
-        with open(f'./ERA5/{filename}', 'w', encoding='utf-8') as file:
-            file.write(content)
+    for var in variables:
+        for year in range(1990, 2024):
+            filename = f'recipe_daily_era5_{var}_{year}.yml'
+            content = template.render(year=year, variable=var)
+            with open(f'{filename}', 'w', encoding='utf-8') as file:
+                file.write(content)
 
-def run_pbs():
-    for year in range(1990, 2023):
-        filename = f'launch_recipe_daily_era5_{year}.pbs'
-        subprocess.run(["qsub", year])
+def run_pbs(variables):
+    for var in variables:
+        for year in range(1990, 2023):
+            filename = f'launch_recipe_daily_era5_{var}_{year}.pbs'
+            subprocess.run(["qsub", filename])
 
 if __name__ == '__main__':
-    generate_pbs_scripts()
-    generate_recipes()
-    run_pbs()
+    var = ["pr"]
+    var = ["psl"]
+    generate_pbs_scripts(variables=var)
+    generate_recipes(variables=var)
+    run_pbs(variables=var)
